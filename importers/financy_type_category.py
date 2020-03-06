4adfad80855859c6c899367ea1d4b574ca1348a5
Andy Lulham <a.lulham@gmail.com>
2020-03-06 09:29:34 +0000

    Don’t explicitly ignore finance type category `0`

--- importers/financy_type_category.py
+++ importers/financy_type_category.py
@@ -1,7 +1,3 @@
-import csv
-
-import requests
-
 from helpers import source_to_xml
 
 
@@ -12,12 +8,4 @@ lookup_no_desc = {
     'name_en': 'name_en',
     'name_fr': 'name_fr',
 }
-r = requests.get(url)
-reader = csv.DictReader(r.iter_lines(decode_unicode=True))
-finance_type_categories = []
-for finance_type_category in reader:
-    if finance_type_category['code'] == '0':
-        continue
-    finance_type_categories.append(finance_type_category)
-source_to_xml('FinanceType-category', None, lookup_no_desc,
-              source_data=finance_type_categories)
+source_to_xml('FinanceType-category', url, lookup_no_desc)
