b67a5adc4ce6d7238f9fe5028896b7558ea3261f
Andy Lulham <a.lulham@gmail.com>
2020-03-06 12:33:05 +0000

    Commit everything (not just updates)

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd codelist_repo
-git add -u || exit 1
+git add . || exit 1
 git commit --author="CodeforIATIbot <57559326+codeforIATIbot@users.noreply.github.com>" -m "${1} update" || exit 1
 git push origin ${1}-update || exit 1
 hub pull-request -r andylolz,markbrough -a andylolz -m "${1} update" -m "Codelist update, sent from [this TravisCI build](https://travis-ci.com/codeforIATI/codelist-updater/builds/${TRAVIS_BUILD_ID})." || exit 1
