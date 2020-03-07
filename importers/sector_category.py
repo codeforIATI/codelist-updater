d557afdb087e5e7beca2e171ad47f8ee5847d21e
Andy Lulham <a.lulham@gmail.com>
2020-03-07 23:08:16 +0000

    Exclude categories that have no children

--- importers/sector_category.py
+++ importers/sector_category.py
@@ -1,13 +1,25 @@
+import csv
+
+import requests
+
 from helpers import source_to_xml
 
 
+sector_url = 'https://raw.githubusercontent.com/datasets/dac-crs-codes/' + \
+             'master/data/sectors.csv'
+r = requests.get(sector_url)
+reader = csv.DictReader(r.iter_lines(decode_unicode=True))
+sector_prefixes = list(set([s['code'][:3] for s in reader]))
+
 url = 'https://raw.githubusercontent.com/datasets/dac-crs-codes/' + \
       'master/data/sector_categories.csv'
-lookup_no_category = {
+lookup = {
     'code': 'code',
     'name_en': 'name_en',
     'name_fr': 'name_fr',
-    'description_en': 'description_en',
-    'description_fr': 'description_fr',
 }
-source_to_xml('SectorCategory', url, lookup_no_category)
+r = requests.get(url)
+reader = csv.DictReader(r.iter_lines(decode_unicode=True))
+categories = [cat for cat in reader
+              if cat['code'] in sector_prefixes]
+source_to_xml('SectorCategory', None, lookup, source_data=categories)
