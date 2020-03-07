6b636f5e11e6c911cf5c547b59a0f2fb95b000f9
Andy Lulham <a.lulham@gmail.com>
2020-03-07 20:05:13 +0000

    If withdrawal data is YYYY-MM, add -01 on the end

--- importers/currency.py
+++ importers/currency.py
@@ -1,11 +1,23 @@
+import csv
+import re
+
+import requests
+
 from helpers import source_to_xml
 
 
 url = 'https://raw.githubusercontent.com/datasets/currency-codes/' + \
       'master/data/codes-all.csv'
-currency_lookup = {
+lookup = {
     'code': 'AlphabeticCode',
     'name_en': 'Currency',
     'withdrawal-date': 'WithdrawalDate',
 }
-source_to_xml('Currency', url, currency_lookup)
+r = requests.get(url)
+reader = csv.DictReader(r.iter_lines(decode_unicode=True))
+currencies = []
+for currency in reader:
+    if re.match(r'^\d{4}-\d{2}$', currency['WithdrawalDate']):
+        currency['WithdrawalDate'] += '-01'
+    currencies.append(currency)
+source_to_xml('Currency', None, lookup, source_data=currencies)
