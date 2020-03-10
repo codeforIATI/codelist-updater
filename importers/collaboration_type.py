bd23d894849150e3a7ee392f6ef97c078851f213
Andy Lulham <a.lulham@gmail.com>
2020-03-10 11:17:42 +0000

    Hack to split CollaborationType name & description

--- importers/collaboration_type.py
+++ importers/collaboration_type.py
@@ -1,11 +1,33 @@
+import csv
+
+import requests
+
 from helpers import source_to_xml
 
 
 url = 'https://raw.githubusercontent.com/datasets/dac-crs-codes/' + \
       'master/data/collaboration_types.csv'
-lookup_no_desc = {
+lookup = {
     'code': 'code',
     'name_en': 'name_en',
     'name_fr': 'name_fr',
+    'description_en': 'description_en',
+    'description_fr': 'description_fr',
 }
-source_to_xml('CollaborationType', url, lookup_no_desc)
+
+r = requests.get(url)
+reader = csv.DictReader(r.iter_lines(decode_unicode=True))
+collaboration_types = []
+
+# hack to split name into name and description.
+# This is just a workaround for an issue with the source data.
+for collaboration_type in reader:
+    collaboration_type['description_fr'] = None
+    for lang in ['en', 'fr']:
+        collaboration_type['description_' + lang] = None
+        name_and_desc = collaboration_type['name_' + lang].split('. ', 1)
+        if len(name_and_desc) > 1:
+            collaboration_type['name_' + lang] = name_and_desc[0] + '.'
+            collaboration_type['description_' + lang] = name_and_desc[1]
+    collaboration_types.append(collaboration_type)
+source_to_xml('CollaborationType', None, lookup, source_data=collaboration_types)
