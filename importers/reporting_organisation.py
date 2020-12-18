a9c1f41ea6889ad20eea7deaf59feca20c901b66
Andy Lulham <a.lulham@gmail.com>
2020-12-18 01:05:36 +0000

    Add reporting org codelist

---
+++ importers/reporting_organisation.py
@@ -0,0 +1,18 @@
+from .helpers import Importer
+
+
+def run():
+    url = 'https://iatiregistry.org/publisher/download_list/csv'
+    lookup = [
+        ('code', 'IATI Organisation Identifier'),
+        ('name_en', 'Publisher'),
+        ('codeforiati:organisation-type', 'Organization Type'),
+        ('codeforiati:hq-country-or-region', 'HQ Country or Region'),
+    ]
+    Importer(
+        'ReportingOrganisation', url, lookup,
+        repo='Unofficial-Codelists')
+
+
+if __name__ == '__main__':
+    run()
