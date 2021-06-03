432ba0cf2955ea4a3b4986b9f7b0612a51873232
Andy Lulham <a.lulham@gmail.com>
2021-06-03 13:53:14 +0100

    Include extra humanitarian plan columns

--- importers/humanitarian_plan.py
+++ importers/humanitarian_plan.py
@@ -5,7 +5,11 @@ def run():
     url = 'https://codeforiati.org/humanitarian-plan-codelist/humanitarian-plan.csv'
     lookup = [
         ('code', 'Plan code'),
-        ('name_en', 'Plan Name'),
+        ('name_en', 'Plan name'),
+        ('codeforiati:plan-type', 'Plan type'),
+        ('codeforiati:plan-year', 'Plan year'),
+        ('codeforiati:plan-start-date', 'Start date'),
+        ('codeforiati:plan-end-date', 'End date'),
     ]
     Importer('HumanitarianPlan', url, lookup)
 
