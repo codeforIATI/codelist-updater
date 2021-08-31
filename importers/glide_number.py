b23383be7800c1824f3c0670f8c0d451a3a19a28
Andy Lulham <a.lulham@gmail.com>
2021-08-31 09:50:12 +0100

    Fixing the column ordering here
    I think the column ordering is important for XML validation. Custom
    namespace stuff should come last

--- importers/glide_number.py
+++ importers/glide_number.py
@@ -7,13 +7,13 @@ def run():
     url = 'https://codeforiati.org/humanitarian-emergency-codelists/glide-emergencies.csv'
     lookup = [
         ('code', 'GLIDE_number'),
+        ('url', 'URL'),
         ('codeforiati:event-code', 'Event_Code'),
         ('codeforiati:event_en', 'Event'),
         ('codeforiati:country-code', 'Country_Code'),
         ('codeforiati:country_en', 'Country'),
         ('codeforiati:date', 'Date'),
         ('codeforiati:glide-serial', 'Glide_Serial'),
-        ('url', 'URL'),
     ]
 
     r = fetch(url)
@@ -21,13 +21,13 @@ def run():
     codes = list(reader)
     codes.insert(0, {
         'GLIDE_number': 'EP-2020-000012-001',
+        'URL': '',
         'Event_Code': 'EP',
         'Event': 'Epidemic',
         'Country_Code': '---',
         'Country': '(Non-Localized)',
         'Date': '2020-01-06',
         'Glide_Serial': '2020-000012',
-        'URL': '',
     })
 
     Importer('GLIDENumber', 'codes', lookup, source_data=codes)
