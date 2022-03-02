5e0ced605f7a6a825c6c11c0aea56c011054f790
Mark Brough <mark.brough@gmail.com>
2022-03-02 10:12:05 +0100

    Update organisation registration agency to keep in sync with org-id.guide

--- importers/organisation_registration_agency.py
+++ importers/organisation_registration_agency.py
@@ -1,6 +1,29 @@
+from .helpers import Importer, fetch
+
 def run():
-    # TODO
-    url = 'http://org-id.guide/download.xml'
+    url = 'http://org-id.guide/download.json'
+    lookup = [
+        ('code', 'code'),
+        ('name_en', 'name/en'),
+        ('description_en', 'description/en'),
+        ('@public-database', 'access/availableOnline'),
+        ('category', 'coverage'),
+        ('url', 'url')
+    ]
+
+    r = fetch(url)
+    data = r.json()
+    registration_agencies = []
+    for registration_agency in data['lists']:
+        registration_agencies.append({
+            'code': registration_agency['code'],
+            'name/en': registration_agency['name']['en'],
+            'description/en': registration_agency['description']['en'],
+            'access/availableOnline': {False: '0', True: '1'}[bool(registration_agency['access'].get('availableOnline', False))],
+            'coverage': ",".join(registration_agency['coverage']) if registration_agency['coverage'] is not None else '',
+            'url': registration_agency['url']
+        })
+    Importer('OrganisationRegistrationAgency', None, lookup, source_data=registration_agencies)
 
 
 if __name__ == '__main__':
